09c5c95f06e4fd3d271381e88d16941c5f2dd738
Xing Meng <mengxingmimi@gmail.com>
2022-04-01 23:39:13 -0700

    add variables

---
+++ script.js
@@ -0,0 +1,27 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChars = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var choices = "";
+
+function generatePassword() {
+  var password = "password";
+  // TODO: add code to generate the password here
+
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
